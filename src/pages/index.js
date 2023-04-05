bd77010b71a0825668cc0796813e4eaf00debd71
Valkon <96625039+Nawija@users.noreply.github.com>
2023-04-05 08:16:01 +0200

    d2

--- src/pages/index.js
+++ src/pages/index.js
@@ -440,6 +440,217 @@ const IndexPage = () => {
                     </div>
                 </div>
             </section>
+
+            <div class="max-w-screen-xl my-6 md:my-24 px-3 mx-auto">
+                <section class="mb-32 text-gray-800">
+                    <div class="block rounded-lg shadow-lg bg-white">
+                        <div class="flex flex-wrap items-center">
+                            <div class="grow-0 shrink-0 basis-auto block w-full lg:flex lg:w-6/12 xl:w-4/12">
+                                <StaticImage
+                                    placeholder="blurred"
+                                    src="https://images.unsplash.com/photo-1615634364452-8daf3851f2f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+                                    alt="Trendy Pants and Shoes"
+                                    class="w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg"
+                                />
+                            </div>
+                            <div class="grow-0 shrink-0 basis-auto w-full lg:w-6/12 xl:w-8/12">
+                                <div class="px-6 my-4 md:py-12 md:px-12">
+                                    <h2 class="text-3xl font-bold mb-6 pb-2">
+                                        Lorem ipsum dolor sit amet
+                                    </h2>
+                                    <p class="text-gray-500 mb-6 pb-2">
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        adipisicing elit. A soluta corporis
+                                        voluptate ab error quam dolores
+                                        doloremque, quae consectetur.
+                                    </p>
+                                    <div class="flex flex-wrap mb-6">
+                                        <div class="w-full lg:w-6/12 xl:w-4/12 mb-4">
+                                            <p class="flex items-center">
+                                                <svg
+                                                    class="w-4 h-4 mr-2 text-green-600"
+                                                    role="img"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 512 512"
+                                                >
+                                                    <path
+                                                        fill="currentColor"
+                                                        d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
+                                                    ></path>
+                                                </svg>
+                                                Support 24/7
+                                            </p>
+                                        </div>
+                                        <div class="w-full lg:w-6/12 xl:w-4/12 mb-4">
+                                            <p class="flex items-center">
+                                                <svg
+                                                    class="w-4 h-4 mr-2 text-green-600"
+                                                    role="img"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 512 512"
+                                                >
+                                                    <path
+                                                        fill="currentColor"
+                                                        d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
+                                                    ></path>
+                                                </svg>
+                                                Analytics
+                                            </p>
+                                        </div>
+                                        <div class="w-full lg:w-6/12 xl:w-4/12 mb-4">
+                                            <p class="flex items-center">
+                                                <svg
+                                                    class="w-4 h-4 mr-2 text-green-600"
+                                                    role="img"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 512 512"
+                                                >
+                                                    <path
+                                                        fill="currentColor"
+                                                        d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
+                                                    ></path>
+                                                </svg>
+                                                Components
+                                            </p>
+                                        </div>
+                                        <div class="w-full lg:w-6/12 xl:w-4/12 mb-4">
+                                            <p class="flex items-center">
+                                                <svg
+                                                    class="w-4 h-4 mr-2 text-green-600"
+                                                    role="img"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 512 512"
+                                                >
+                                                    <path
+                                                        fill="currentColor"
+                                                        d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
+                                                    ></path>
+                                                </svg>
+                                                Updates
+                                            </p>
+                                        </div>
+                                        <div class="w-full lg:w-6/12 xl:w-4/12 mb-4">
+                                            <p class="flex items-center">
+                                                <svg
+                                                    class="w-4 h-4 mr-2 text-green-600"
+                                                    role="img"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 512 512"
+                                                >
+                                                    <path
+                                                        fill="currentColor"
+                                                        d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
+                                                    ></path>
+                                                </svg>
+                                                Reports
+                                            </p>
+                                        </div>
+                                        <div class="w-full lg:w-6/12 xl:w-4/12 mb-4">
+                                            <p class="flex items-center">
+                                                <svg
+                                                    class="w-4 h-4 mr-2 text-green-600"
+                                                    role="img"
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    viewBox="0 0 512 512"
+                                                >
+                                                    <path
+                                                        fill="currentColor"
+                                                        d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"
+                                                    ></path>
+                                                </svg>
+                                                Mobile
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <button
+                                        type="button"
+                                        class="inline-block px-7 py-3 bgLogoColor text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-gray-800 hover:shadow-lg focus:bg-gray-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out"
+                                        data-mdb-ripple="true"
+                                        data-mdb-ripple-color="light"
+                                    >
+                                        Contact Now
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                {/* <section class="text-gray-600 body-font relative bg-white w-full">
+                    <div class="px-5 py-24 mx-auto">
+                        <div class="flex flex-col text-center w-full mb-12">
+                            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+                                Contact Us
+                            </h1>
+                            <p class="lg:w-2/3 mx-auto leading-relaxed text-base">
+                                Whatever cardigan tote bag tumblr hexagon
+                                brooklyn asymmetrical gentrify.
+                            </p>
+                        </div>
+                        <div class="lg:w-1/2 md:w-2/3 mx-auto">
+                            <div class="flex flex-wrap -m-2">
+                                <div class="p-2 w-1/2">
+                                    <div class="relative">
+                                        <label
+                                            for="name"
+                                            class="leading-7 text-sm text-gray-600"
+                                        >
+                                            Name
+                                        </label>
+                                        <input
+                                            type="text"
+                                            id="name"
+                                            name="name"
+                                            class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-gray-800 focus:bg-white focus:ring-2 focus:ring-gray-300 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                        />
+                                    </div>
+                                </div>
+                                <div class="p-2 w-1/2">
+                                    <div class="relative">
+                                        <label
+                                            for="email"
+                                            class="leading-7 text-sm text-gray-600"
+                                        >
+                                            Email
+                                        </label>
+                                        <input
+                                            type="email"
+                                            id="email"
+                                            name="email"
+                                            class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                        />
+                                    </div>
+                                </div>
+                                <div class="p-2 w-full">
+                                    <div class="relative">
+                                        <label
+                                            for="message"
+                                            class="leading-7 text-sm text-gray-600"
+                                        >
+                                            Message
+                                        </label>
+                                        <textarea
+                                            id="message"
+                                            name="message"
+                                            class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                                        ></textarea>
+                                    </div>
+                                </div>
+                                <div class="p-2 w-full">
+                                    <button class="flex mx-auto text-white bgLogoColor border-0 py-2 px-8 focus:outline-none hover:bg-gray-800 rounded text-lg">
+                                        Send
+                                    </button>
+                                </div>
+                                <div class="p-2 w-full pt-2 mt-2 border-t border-gray-200 text-center">
+                                    <Link to="mailto:example@email.com" class="linkColor hoverLink">
+                                        example@email.com
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section> */}
+            </div>
         </Layout>
     );
 };
