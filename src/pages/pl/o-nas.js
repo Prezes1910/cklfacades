741c48df515185994afc5143b19c1f0a1689d4a4
Valkon <96625039+Nawija@users.noreply.github.com>
2023-04-15 08:03:06 +0200

    Update o-nas.js

--- src/pages/pl/o-nas.js
+++ src/pages/pl/o-nas.js
@@ -1,25 +1,28 @@
 import * as React from "react";
 import Layout from "../../components/pl/layoutPL";
-import { StaticImage } from "gatsby-plugin-image";
 import Seo from "../../components/pl/seoPL";
-// import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
-const oNas = () => (
+
+const AboutUsPL = () => (
     <Layout>
         <section className="relative object-bottom flex items-center min-h-[16rem] overflow-hidden h-24 justify-center w-full">
             <div className="w-full -mt-4 -z-20">
                 <StaticImage
                     className=" object-bottom w-[220%] md:w-full"
                     quality={100}
+                    alt="fasades"
                     src="../../assets/hero.png"
                     placeholder="blurred"
                 />
             </div>
         </section>
-
+        <section className="h-[40vh] w-full flex flex-col items-center justify-center">
+            
+        </section>
     </Layout>
 );
 
 export const Head = () => <Seo titlePL="O Nas" />;
 
-export default oNas;
+export default AboutUsPL;
